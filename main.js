5ecf4585812ec0c86642cf3907238a0a5e947e01
habberzabib <145040111+habberzabib@users.noreply.github.com>
2023-12-06 12:00:06 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -6,22 +6,168 @@ let ctx = cnv.getContext("2d");
 cnv.width = 600;
 cnv.height = 400;
 
-//Global Variables
+// //Global Variables
+// let rectX = 100;
+// let rectSize = 80;
+
+// requestAnimationFrame(loop);
+// function loop() {
+//   //Update variables
+//   rectX = Math.random();
+
+//   //Draw bg
+//   ctx.fillStyle = "white";
+//   ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+//   //Draw a square
+//   ctx.fillStyle = "orange";
+//   ctx.fillRect(rectX, 150, rectSize, rectSize);
+
+//   requestAnimationFrame(loop);
+// }
+
+// //Animation basics
+// let x = 100;
+
+// requestAnimationFrame(draw);
+// function draw() {
+//   //Update elements on canvas
+//   x++;
+
+//   //Draw canvas
+//   ctx.fillStyle = "white";
+//   ctx.fillRect(0, 0, cnv.width, cnv.height); //Bg
+
+//   ctx.fillStyle = "green";
+//   ctx.fillRect(x, 100, 50, 50); //Rectangle
+
+//   requestAnimationFrame(draw);
+// }
+
+// //Run animation till a certain value/updating the variable
+// requestAnimationFrame(draw);
+// function draw() {
+//   if (x < 400) {
+//     x++;
+//   }
+
+//   ctx.fillStyle = "white";
+//   ctx.fillRect(0, 0, cnv.width, cnv.height); //Bg
+
+//   ctx.fillStyle = "green";
+//   ctx.fillRect(x, 100, 50, 50); //Rectangle
+
+//   requestAnimationFrame(draw);
+// }
+
+// //Running animation for a certain amount of time
+
+// let a = 100;
+// let frameCount = 0;
+
+// requestAnimationFrame(draw);
+// function draw() {
+//   frameCount++;
+
+//   if (frameCount < 120) {
+//     a++;
+//   }
+
+//   ctx.fillStyle = "white";
+//   ctx.fillRect(0, 0, cnv.width, cnv.height); //Bg
+
+//   ctx.fillStyle = "green";
+//   ctx.fillRect(a, 100, 50, 50); //rect
+
+//   requestAnimationFrame(draw);
+// }
+
+// //Reset animation
+
+// let b = 100;
+
+// requestAnimationFrame(animate);
+// function animate() {
+//   b++;
+
+//   if (b > 300) {
+//     b = 100;
+//   }
+
+//   ctx.fillStyle = "white";
+//   ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+//   ctx.fillStyle = "green";
+//   ctx.fillRect(b, 100, 50, 50);
+
+//   requestAnimationFrame(animate);
+// }
+
+//global variables
 let rectX = 100;
 let rectSize = 80;
+let rectBlue = 255;
+let rectRed = 0;
 
+let animate = false;
+
+//main program loop (60 fps)
 requestAnimationFrame(loop);
 function loop() {
-  //Update variables
-  rectX = Math.random();
+  //update variables
+  if (animate) {
+    rectX += 1;
+    rectSize += Math.random() * 2 - 1;
+    rectBlue--;
+    rectRed++;
+  }
 
-  //Draw bg
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, cnv.width, cnv.height);
 
-  //Draw a square
-  ctx.fillStyle = "orange";
+  ctx.fillStyle = "rgb(" + rectRed + ", 0," + rectBlue + ")";
   ctx.fillRect(rectX, 150, rectSize, rectSize);
 
   requestAnimationFrame(loop);
+}
+
+//event stuff
+
+// document.addEventListener("mousedown", mouseDownHandler);
+// document.addEventListener("mouseup", mouseUpHandler);
+
+// function mouseDownHandler() {
+//   mouseIsPressed = true;
+// }
+// function mouseUpHandler() {
+//   mouseIsPressed = false;
+// }
+
+//OR
+
+// document.addEventListener("click", mouseClickHandler);
+
+// function mouseClickHandler() {
+//   if (animate) {
+//     animate = false;
+//   } else {
+//     animate = true;
+//   }
+// }
+
+//OR
+
+// document.addEventListener("keydown", keyDownHandler);
+
+// function keyDownHandler() {
+//   animate = !animate;
+// }
+
+document.addEventListener("keydown", keyDownHandler);
+
+function keyDownHandler(event) {
+  console.log(event.code);
+  if (event.code == "Space") {
+    animate = !animate;
+  }
 }
