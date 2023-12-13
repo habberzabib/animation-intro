6a0faffbaa46c04f8c578cb370626341c6e1f5f2
habberzabib <145040111+habberzabib@users.noreply.github.com>
2023-12-13 10:56:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Animation Intro
 
-//Setup Cana=vas and Graphics Context
+//Setup Canvas and Graphics Context
 let cnv = document.getElementById("myCanvas");
 let ctx = cnv.getContext("2d");
 cnv.width = 600;
@@ -103,36 +103,44 @@ cnv.height = 400;
 //   requestAnimationFrame(animate);
 // }
 
-//global variables
+// //MOUSE & KEYBOARD INPUT
+
+//OR
+
+//Global Variables
 let rectX = 100;
 let rectSize = 80;
-let rectBlue = 255;
+let rectBlue = 225;
 let rectRed = 0;
 
+//let spaceKeyIsPressed = false;
 let animate = false;
 
-//main program loop (60 fps)
+//Main Program Loop (60 FPS)
 requestAnimationFrame(loop);
 function loop() {
-  //update variables
+  //Update Variables
+  //if (spaceKeyIsPressed) {
   if (animate) {
     rectX += 1;
-    rectSize += Math.random() * 2 - 1;
+    rectSize += Math.random() * 2 - 1; //-1 to 1
     rectBlue--;
     rectRed++;
   }
-
+  //Draw bg
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, cnv.width, cnv.height);
 
+  //Draw a square
   ctx.fillStyle = "rgb(" + rectRed + ", 0," + rectBlue + ")";
   ctx.fillRect(rectX, 150, rectSize, rectSize);
 
   requestAnimationFrame(loop);
 }
 
-//event stuff
+//EVENT STUFF
 
+// // Click and hold mouse to animate
 // document.addEventListener("mousedown", mouseDownHandler);
 // document.addEventListener("mouseup", mouseUpHandler);
 
@@ -143,31 +151,60 @@ function loop() {
 //   mouseIsPressed = false;
 // }
 
-//OR
+// // Click to start and click to stop animation
 
-// document.addEventListener("click", mouseClickHandler);
+document.addEventListener("click", mouseClickHandler);
 
-// function mouseClickHandler() {
-//   if (animate) {
-//     animate = false;
-//   } else {
-//     animate = true;
-//   }
-// }
+function mouseClickHandler() {
+  if (animate) {
+    animate = false;
+  } else {
+    animate = true;
+  }
+}
 
-//OR
-
-// document.addEventListener("keydown", keyDownHandler);
-
-// function keyDownHandler() {
-//   animate = !animate;
-// }
+// OR
 
 document.addEventListener("keydown", keyDownHandler);
 
-function keyDownHandler(event) {
-  console.log(event.code);
-  if (event.code == "Space") {
-    animate = !animate;
-  }
+function keyDownHandler() {
+  animate = !animate;
 }
+
+// //Click any key to start and stop animation
+
+// document.addEventListener("keydown", keyDownHandler);
+
+// function keyDownHandler(event) {
+//   console.log(event.code);
+//    animate = !animate;
+//   }
+// }
+
+// //Use a special key (ex. space key)
+
+// document.addEventListener("keydown", keyDownHandler);
+
+// function keyDownHandler(event) {
+//   console.log(event.code);
+//   if (event.code == "Space") {
+//     animate = !animate;
+//   }
+// }
+
+// //Click and hold key to
+
+// document.addEventListener("keydown", keyDownHandler);
+// document.addEventListener("keyup", keyUpHandler);
+
+// function keyDownHandler(event) {
+//   if (event.code == "Space") {
+//     spaceKeyIsPressed = true;
+//   }
+// }
+
+// function keyUpHandler(event) {
+//   if (event.code == "Space") {
+//     spaceKeyIsPressed = false;
+//   }
+// }
